75c3518c8e349438fe56d7fda7633e5e20e63cd0
eagle studios <austincolton624@gmail.com>
2025-12-18 15:11:24 -0500

    Add files via upload

---
+++ script installer.py
@@ -0,0 +1,30 @@
+import subprocess
+import time
+
+# List of packages you want to install
+packages = [
+    "requests",
+    "numpy",
+    "pillow",
+    "flask",
+    "rich",
+    "pytest",
+    "pandas",
+    "scipy"  
+]
+
+def install_packages(package_list):
+    for pkg in package_list:
+        print(f"\nInstalling {pkg}...")
+        
+        # Open cmd and run pip install
+        subprocess.run(
+            f'cmd /c "pip install {pkg}"',
+            shell=True
+        )
+        
+        time.sleep(1)  # small delay between installs
+
+if __name__ == "__main__":
+    install_packages(packages)
+    print("\nAll installations complete!")
